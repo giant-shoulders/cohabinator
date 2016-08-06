e3bc8863e5ff8165a5f38895b498c8f8a2cfe48c
Alex Howard <thezanke@gmail.com>
2016-08-06 01:29:55 -0400

    Working boilerplate

---
+++ src/reducers/dogsReducer.js
@@ -0,0 +1,7 @@
+const initialState = [
+  { id: 1, name: 'Bruce' },
+];
+
+const dogsReducer = (state = initialState) => state;
+
+export default dogsReducer;
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,7 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({});
+import dogsReducer from './dogsReducer';
+
+export default combineReducers({
+  dogs: dogsReducer,
+});
