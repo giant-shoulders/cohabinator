2e945b96acae01ce99a19a8825a7fd980708ff42
Alex Howard <thezanke@gmail.com>
2016-08-16 14:10:52 -0400

    Added logo, generated new build

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,10 +1,13 @@
 import './AppLayout.css';
 
+import logo from '../../assets/images/cohabinator.png';
+
 import React from 'react';
 
 const AppLayout = () => (
   <div className="container-body">
     <div>
+      <img src={logo} alt="Cohabinator Logo" />
       <h1>Cohabinator</h1>
       <h5>Coming soon.</h5>
     </div>
